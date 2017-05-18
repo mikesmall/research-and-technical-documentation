2b98e3d3c614c34696ed83e9db5aa46a00da75c5
mikesmall <michael.l.a.small@gmail.com>
2017-05-18 15:47:03 -0400

    Finished 'Array' section

--- work.rb
+++ work.rb
@@ -32,4 +32,62 @@ puts string.start_with?("That's", "What's")
 # => true
 string = "I hate sand"
 puts string.start_with?("You", "Don't")
-# => 
+# => false
+# Feels pretty self-explanatory. True if it's there, false if it's not.
+
+# Array: first
+# first way: arguments: none
+#            return: obj or nil
+# second way: arguments: n
+#             return: new_ary
+array = ["Tommy", "Dee Dee", "Joey", "Johnny"]
+puts array.first
+# => Tommy
+puts array.first(2)
+# => Tommy
+# => Dee Dee
+puts array.first(3)
+# => Tommy
+# => Dee Dee
+# => Joey
+
+# Array: delete_at
+# arguments: index
+# return: obj or nil
+array = ["Sporty", "Scary", "Baby", "Ginger", "Posh"]
+puts array.delete_at(3)
+# => Ginger
+puts array
+# => Sporty
+# => Scary
+# => Baby
+# => Posh
+# delete_at removes a specified array element, announcing which element it removed.
+
+# Array: delete
+# arguments: obj
+# return: item or nil
+array = ["cat", "dog", "cat", "bird", "stone", "cat"]
+array.delete("cat")
+puts array
+# > dog
+# > bird
+# > stone
+
+# Array: pop
+#   first way: arguments: none
+#              return: obj or nil
+#   second way: arguments: n
+#               return: new_ary
+array = ["MegaBus", "Greyhound", "Via", "GO", "TTC", "MiWay"]
+array.pop
+puts array
+# => MegaBus
+# => Greyhound
+# => Via
+# => GO
+# => TTC
+array.pop(3)
+puts array
+# => MegaBus
+# => Greyhound 
